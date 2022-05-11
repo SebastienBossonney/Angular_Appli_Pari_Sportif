30c26d2bf5d871d019121d1027b1858e86507ef2
SebastienBossonney <bossonney.s@hotmail.fr>
2022-05-11 14:10:09 +0200

    Update

--- appli-pari-sportif/src/app/compte-utilisateur/compte-utilisateur.component.ts
+++ appli-pari-sportif/src/app/compte-utilisateur/compte-utilisateur.component.ts
@@ -23,7 +23,7 @@ export class CompteUtilisateurComponent implements OnInit {
 
   constructor()  {}
 
-  ngOnInit(): void {
+  ngOnInit(){
 
 
     // this.utilisateurService.getUtilisateur().subscribe(utilisateur =>{
