99204712a9d2b832def97332d60b73f5e69c06ac
Nael WENTZLER <wtznael@gmail.com>
2022-05-11 11:57:26 +0200

    Fixed main

---
+++ appli-pari-sportif/src/app/parier/pari-sport-match/pari-sport-match.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PariSportMatchComponent } from './pari-sport-match.component';
+
+describe('PariSportMatchComponent', () => {
+  let component: PariSportMatchComponent;
+  let fixture: ComponentFixture<PariSportMatchComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PariSportMatchComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PariSportMatchComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
