703d2b4976c6cf2e961c0852766894eec69d9c0d
mafeg87 <104212170+mafeg87@users.noreply.github.com>
2022-05-13 14:40:12 +0200

    5

--- appli-pari-sportif/src/app/parier/pari-sport-match/pari-sport-match.component.ts
+++ appli-pari-sportif/src/app/parier/pari-sport-match/pari-sport-match.component.ts
@@ -77,6 +77,6 @@ export class PariSportMatchComponent implements OnInit, OnDestroy {
     }
 
     parier(){
-      
+
     }
 }
---
+++ appli-pari-sportif/src/app/parier/pari.ts
@@ -0,0 +1,11 @@
+export interface Pari {
+
+  id: number;
+  montantJoue: number;
+  datePari: Date;
+  heurePari: Date;
+  resultat: string;
+  montantResultat: number;
+  utilisateurId: number;
+  coteId : number;
+}
