f30e3939e824ff6d4c9b6e099426300d4b484a83
Nael WENTZLER <wtznael@gmail.com>
2022-05-18 16:32:11 +0200

    commit updated final version

--- appli-pari-sportif/src/app/historique-pari-foot/historique-pari-foot.component.ts
+++ appli-pari-sportif/src/app/historique-pari-foot/historique-pari-foot.component.ts
@@ -13,8 +13,6 @@ export class HistoriquePariFootComponent implements OnInit {
 
   user!: Utilisateur;
   listPari!: Pari[];
-  lesParis: boolean=false;
-  utilisateurId!: number;
 
   constructor(private pariService: PariService, private utilisateurService: UtilisateurService) { }
 
@@ -27,14 +25,6 @@ export class HistoriquePariFootComponent implements OnInit {
     console.log(this.listPari.length);});
   }
 
-voirParis(){
 
-    if (this.lesParis) {
-      this.lesParis = false;
-    } else {
-      this.lesParis = true;
-    }
-
-  }
 
 }
